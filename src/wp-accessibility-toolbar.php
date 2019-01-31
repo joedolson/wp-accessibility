f06b8f626b16d7f8b972533699378314f60eac48
Joe Dolson <design@joedolson.com>
2019-01-31 13:49:14 -0600

    Bug fix: fix widget output
    Widget output should not be styled

--- src/wp-accessibility-toolbar.php
+++ src/wp-accessibility-toolbar.php
@@ -9,7 +9,23 @@
  * @link     https://www.joedolson.com/wp-accessibility/
  */
 
-add_shortcode( 'wpa_toolbar', 'wpa_toolbar_html' );
+add_shortcode( 'wpa_toolbar', 'wpa_toolbar_shortcode' );
+/**
+ * Output Toolbar shortcode
+ *
+ * @param array $atts Shortcode attributes.
+ * @param string $content Contained content.
+ *
+ * @return string
+ */
+function wpa_toolbar_shortcode( $atts, $content ) {
+	$args = shortcode_atts( array(
+		'type'    => 'widget',
+		'control' => 'button',
+	), $atts, 'wpa_toolbar' );
+
+	return wpa_toolbar_html( $args['type'], $args['control'] );
+}
 /**
  * Generate Toolbar as HTML.
  *
@@ -17,7 +33,7 @@ add_shortcode( 'wpa_toolbar', 'wpa_toolbar_html' );
  *
  * @return string HTML.
  */
-function wpa_toolbar_html( $type = 'widget' ) {
+function wpa_toolbar_html( $type = 'widget', $control = 'button' ) {
 	$contrast         = __( 'Toggle High Contrast', 'wp-accessibility' );
 	$grayscale        = __( 'Toggle Grayscale', 'wp-accessibility' );
 	$fontsize         = __( 'Toggle Font size', 'wp-accessibility' );
@@ -28,20 +44,20 @@ function wpa_toolbar_html( $type = 'widget' ) {
 	$is_rtl           = ( is_rtl() ) ? ' rtl' : ' ltr';
 	$is_right         = ( 'on' == get_option( 'wpa_toolbar_right' ) ) ? ' right' : ' left';
 	$toolbar_type     = ( 'widget' == $type ) ? 'a11y-toolbar-widget' : 'a11y-toolbar';
-	$control_type     = ( 'button' != $type ) ? 'a href="#" role="button"' : 'button type="button"'; // button control does not work in Edge.
-	$closure          = ( 'button' != $type ) ? 'a' : 'button';  // button control does not work in Edge.
+	$control_type     = ( 'button' != $control ) ? 'a href="#" role="button"' : 'button type="button"'; // button control does not work in Edge.
+	$closure          = ( 'button' != $control ) ? 'a' : 'button';  // button control does not work in Edge.
 	$toolbar          = '
 <!-- a11y toolbar widget -->
 <div class="' . $responsive . ' ' . $is_rtl . ' ' . $is_right . ' ' . $toolbar_type . '">
 	<ul>';
 	if ( $enable_contrast ) {
-		$toolbar .= '<li><' . $control_type . ' class="a11y-toggle-contrast toggle-contrast" id="is_normal_contrast" aria-pressed="false"><span class="offscreen">' . $contrast . '</span><span class="aticon aticon-adjust" aria-hidden="true"></span></' . $closure . '></li>';
+		$toolbar .= '<li><' . $control_type . ' class="a11y-toggle-contrast toggle-contrast" id="is_normal_contrast" aria-pressed="false"><span class="offscreen">' . $contrast . '</span> <span class="aticon aticon-adjust" aria-hidden="true"></span></' . $closure . '></li>';
 	}
 	if ( $enable_grayscale ) {
-		$toolbar .= '<li><' . $control_type . ' class="a11y-toggle-grayscale toggle-grayscale" id="is_normal_color" aria-pressed="false"><span class="offscreen">' . $grayscale . '</span><span class="aticon aticon-tint" aria-hidden="true"></span></' . $closure . '></li>';
+		$toolbar .= '<li><' . $control_type . ' class="a11y-toggle-grayscale toggle-grayscale" id="is_normal_color" aria-pressed="false"><span class="offscreen">' . $grayscale . '</span> <span class="aticon aticon-tint" aria-hidden="true"></span></' . $closure . '></li>';
 	}
 	if ( $enable_fontsize ) {
-		$toolbar .= '<li><' . $control_type . ' class="a11y-toggle-fontsize toggle-fontsize" id="is_normal_fontsize" aria-pressed="false"><span class="offscreen">' . $fontsize . '</span><span class="aticon aticon-font" aria-hidden="true"></span></' . $closure . '></li>';
+		$toolbar .= '<li><' . $control_type . ' class="a11y-toggle-fontsize toggle-fontsize" id="is_normal_fontsize" aria-pressed="false"><span class="offscreen">' . $fontsize . '</span> <span class="aticon aticon-font" aria-hidden="true"></span></' . $closure . '></li>';
 	}
 	$toolbar .= '
 	</ul>
@@ -64,7 +80,7 @@ function wpa_toolbar_js() {
 	$responsive = ( 'on' == get_option( 'wpa_toolbar_mobile' ) ) ? 'a11y-responsive ' : 'a11y-non-responsive ';
 
 	if ( preg_match( '/Edge/i', $user_agent ) ) {
-		echo wpa_toolbar_html( 'js' );
+		echo wpa_toolbar_html( 'js', 'a' );
 		echo "
 <script type='text/javascript'>
 //<![CDATA[
