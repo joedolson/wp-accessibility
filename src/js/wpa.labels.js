cfa7843a99c96aa48ba69bf07e4866d9f7f4f47b
Joe Dolson <design@joedolson.com>
2019-03-03 15:33:31 -0600

    Don't double label implicitly labeled fields
    This won't fix implicitly labeled fields with no accessible name, but it
    also won't double-label fields that are labeled in a valid manner.

--- src/js/wpa.labels.js
+++ src/js/wpa.labels.js
@@ -6,15 +6,17 @@
 		} else {
 			var field = $( 'input[name=' + value + ']' );
 		}
-		var form_id = field.attr( 'id' );			
+		var form_id = field.attr( 'id' );
+		var implicit = $( field ).parent( 'label' );
 		if ( form_id ) {
 			var label = $( 'label[for=' + form_id + ']' );
-			var implicit = $( field ).parent( 'label' );
 			if ( !label.length && !implicit.length ) {
 				field.before( "<label for='" + form_id + "' class='wpa-screen-reader-text'>" + wpalabels[value] + "</label>" );
 			}
 		} else {
-			field.attr( 'id', 'wpa_label_' + value ).before( "<label for='wpa_label_" + value + "' class='wpa-screen-reader-text'>" + wpalabels[value] + "</label>" );
-		}		
+			if ( !implicit.length ) {
+				field.attr( 'id', 'wpa_label_' + value ).before( "<label for='wpa_label_" + value + "' class='wpa-screen-reader-text'>" + wpalabels[value] + "</label>" );
+			}
+		}
 	});
 }(jQuery));
