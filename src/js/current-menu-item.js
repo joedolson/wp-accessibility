1fc976965ddc8317c59b12284d25c23846894d43
Joe Dolson <design@joedolson.com>
2019-05-26 17:59:20 -0500

    Can only operate on element; not a collection
    reverting; this aria-current check works, it just could change the
    value. Shouldn't matter, however.

--- src/js/current-menu-item.js
+++ src/js/current-menu-item.js
@@ -1,9 +1,5 @@
 (function ($) {
 	$(function() {
-		var current_item;
-		current_item = $( '.current-menu-item a, .current_page_item a' );
-		if ( ! current_item.hasAttribute( 'aria-current' ) ) {
-			current_item.attr( 'aria-current', 'page' ).append( "<span class='screen-reader-text'>(current)</span>" );
-		}
+		$( '.current-menu-item a, .current_page_item a' ).attr( 'aria-current', 'page' ).append( "<span class='screen-reader-text'>(current)</span>" );
 	});
 }(jQuery));
