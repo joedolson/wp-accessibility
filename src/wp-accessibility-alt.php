61219a6140ccab0454b743905fbf66b9d45b06c1
Joe Dolson <design@joedolson.com>
2019-11-13 17:03:57 -0600

    Sniff fixes

--- src/wp-accessibility-alt.php
+++ src/wp-accessibility-alt.php
@@ -38,7 +38,7 @@ function wpa_media_columns( $columns ) {
  */
 function wpa_media_value( $column, $id ) {
 	if ( 'wpa_data' === $column ) {
-		$mime = get_post_mime_type( $id );
+		$mime           = get_post_mime_type( $id );
 		$invalid_values = array(
 			'""',
 			"''",
@@ -54,7 +54,7 @@ function wpa_media_value( $column, $id ) {
 				} else {
 					if ( true === $no_alt ) {
 						echo '<span class="ok"><span class="dashicons dashicons-yes" aria-hidden="true"></span> ' . __( 'Decorative', 'wp-accessibility' ) . '</span>';
-					} else if ( in_array( $alt, $invalid_values, true ) ) {
+					} elseif ( in_array( $alt, $invalid_values, true ) ) {
 						echo '<span class="missing"><span class="dashicons dashicons-no" aria-hidden="true"></span> ' . __( 'Invalid', 'wp-accessibility' ) . '</span>';
 					} else {
 						echo '<span class="ok"><span class="dashicons dashicons-yes" aria-hidden="true"></span> ' . __( 'Has <code>alt</code>', 'wp-accessibility' ) . '</span>';
