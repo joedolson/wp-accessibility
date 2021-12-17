71bdab2fe63e21ec1d86c33b45b827237e2e8802
Joe Dolson <design@joedolson.com>
2021-12-17 13:56:15 -0600

    Improve label detection
    Don't execute logic if field not present; ignore adminbar search field; support aria-label or aria-labelledby for fields.

--- src/js/wpa.labels.js
+++ src/js/wpa.labels.js
@@ -4,18 +4,34 @@
 		if ( value == 'comment' ) {
 			var field = $( 'textarea[name=' + value + ']' );
 		} else {
-			var field = $( 'input[name=' + value + ']' );
+			var field = $( 'input[name=' + value + ']' ).not( '#adminbar-search' );
 		}
-		var form_id = field.attr( 'id' );
-		var implicit = $( field ).parent( 'label' );
-		if ( form_id ) {
-			var label = $( 'label[for=' + form_id + ']' );
-			if ( !label.length && !implicit.length ) {
-				field.before( "<label for='" + form_id + "' class='wpa-screen-reader-text'>" + wpalabels[value] + "</label>" );
+		if ( 0 !== field.length ) {
+			var form_id = field.attr( 'id' );
+			var implicit = $( field ).parent( 'label' );
+			var aria = $( field ).attr( 'aria-label' );
+			var ariaId = $( field ).attr( 'aria-labelledby' );
+			var ariaTarget = {};
+			if ( ariaId ) {
+				ariaTarget = $( '#' + ariaId );
 			}
-		} else {
-			if ( !implicit.length ) {
-				field.attr( 'id', 'wpa_label_' + value ).before( "<label for='wpa_label_" + value + "' class='wpa-screen-reader-text'>" + wpalabels[value] + "</label>" );
+			var hasAria   = ( '' == aria || 'undefined' == typeof( aria ) ) ? false : true;
+			var hasAriaId = ( '' == ariaId || 'undefined' == typeof( ariaId ) ) ? false : true;
+			// Add label if aria label empty, aria labelledby empty, or aria reference ID does not exist.
+			if ( ( ! hasAria && ! hasAriaId ) || ( ! hasAria && ( hasAriaId && 0 === ariaTarget.length ) ) ) {
+				if ( hasAriaId && 0 === ariaTarget.length ) {
+					console.log( 'aria-labelledby target ID does not exist: ', ariaId );
+				}
+				if ( form_id ) {
+					var label = $( 'label[for=' + form_id + ']' );
+					if ( !label.length && !implicit.length ) {
+						field.before( "<label for='" + form_id + "' class='wpa-screen-reader-text'>" + wpalabels[value] + "</label>" );
+					}
+				} else {
+					if ( !implicit.length ) {
+						field.attr( 'id', 'wpa_label_' + value ).before( "<label for='wpa_label_" + value + "' class='wpa-screen-reader-text'>" + wpalabels[value] + "</label>" );
+					}
+				}
 			}
 		}
 	});
