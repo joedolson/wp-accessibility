73fffd9eecde01bb3dcb07717fb083421ea6441e
Joe Dolson <design@joedolson.com>
2022-01-14 17:28:07 -0600

    Update wp-accessibility.php
    Other copyright

--- src/wp-accessibility.php
+++ src/wp-accessibility.php
@@ -21,7 +21,7 @@
  */
 
 /*
-	Copyright 2012-2021  Joe Dolson (email : joe@joedolson.com)
+	Copyright 2012-2022  Joe Dolson (email : joe@joedolson.com)
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
