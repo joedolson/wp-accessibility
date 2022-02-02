69465f9fcc043c5c60857d6abf1561d1264486fd
Joe Dolson <design@joedolson.com>
2022-02-02 11:34:11 -0600

    Incorrect variable called for class
    Fixes responsive toolbar

--- src/js/wpa-toolbar.js
+++ src/js/wpa-toolbar.js
@@ -13,7 +13,7 @@
 		insert_a11y_toolbar += '<li class="a11y-toolbar-list-item"><button type="button" class="a11y-toggle-fontsize toggle-fontsize" id="is_normal_fontsize" aria-pressed="false"><span class="offscreen">' + wpa.fontsize + '</span><span class="aticon aticon-font" aria-hidden="true"></span></button></li>';
 	}
 	insert_a11y_toolbar += '</ul>';
-	a11y_toolbar.classList.add( wpa.response, 'a11y-toolbar', wpa.is_rtl, wpa.is_right );
+	a11y_toolbar.classList.add( wpa.responsive, 'a11y-toolbar', wpa.is_rtl, wpa.is_right );
 	a11y_toolbar.innerHTML = insert_a11y_toolbar;
 
 	var insertionPoint = document.querySelector( wpa.location );
