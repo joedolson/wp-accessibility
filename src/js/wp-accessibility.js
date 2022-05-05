519f2a79041e85e7278f3ecc0728e080f59d247e
Joe Dolson <design@joedolson.com>
2022-05-05 17:57:05 -0500

    Make faux buttons focusable.

--- src/js/wp-accessibility.js
+++ src/js/wp-accessibility.js
@@ -28,8 +28,15 @@
 	}
 
 	if ( wpa.tabindex ) {
+		// Remove tabindex from elements that should be natively focusable.
 		var focusable = $('input,a,select,textarea,button').not('a:not([href])');
 		focusable.removeAttr('tabindex');
+
+		// Add tabindex to elements that appear active but are not natively focusable.
+		var fakeButtons = $('div[role="button"]').not('div[tabindex]' );
+		var buttonLinks = $('a[role="button"]').not('a[tabindex],a[href]');
+		fakeButtons.attr( 'tabindex', '0' );
+		buttonLinks.attr( 'tabindex', '0' );
 	}
 
 	if ( wpa.underline.enabled ) {
