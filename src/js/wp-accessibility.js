23a54e66924ad5dbe8795ea15f293b39aeff4a49
Joe Dolson <design@joedolson.com>
2022-10-25 15:14:50 -0500

    First draft of removing title attributes in JS

--- src/js/wp-accessibility.js
+++ src/js/wp-accessibility.js
@@ -28,6 +28,64 @@
 		}
 	}
 
+	if ( wpa.titles ) {
+		const els = document.querySelectorAll( 'img, a, input, textarea, select, button' );
+		els.forEach((el) => {
+			var title = el.getAttribute( 'title' );
+			if ( title && '' !== title ) {
+				switch ( el.tagName ) {
+					case 'IMG':
+						// If image has alt, remove title. If not, set title as alt.
+						var alt = el.getAttribute( 'alt' );
+						if ( ! alt || '' === alt ) {
+							el.setAttribute( 'alt', title );
+							el.removeAttribute( 'title' );
+						} else {
+							el.removeAttribute( 'title' );
+						}
+						break;
+					case 'A':
+					case 'BUTTON':
+						// If link or button has contained text or an img with alt, remove title. Otherwise, set title as aria-label unless element already has aria-label.
+						var linkText = wpaElementText(el);
+						if ( ! linkText || '' === linkText ) {
+							var ariaLabel = el.getAttribute( 'aria-label' );
+							if ( ! ariaLabel || '' === ariaLabel ) {
+								el.setAttribute( 'aria-label', title );
+								el.removeAttribute( 'title' );
+							}
+						} else {
+							el.removeAttribute( 'title' );
+						}
+						break;
+					case 'INPUT':
+					case 'SELECT':
+					case 'TEXTAREA':
+						// If input field has an aria-label, aria-labelledby, associated label, or wrapping label, remove title. Else, add title as aria-label.
+						var ariaLabel        = el.getAttribute( 'aria-label' );
+						var ariaLabelled     = el.getAttribute( 'aria-labelledby' );
+						var ariaLabeller     = ( ariaLabelled ) ? document.getElementById( ariaLabelled ) : false;
+						var labelId          = el.getAttribute( 'id' );
+						var label            = ( labelId ) ? document.querySelector( 'for="' + labelId + '"' ) : false;
+						var parentLabel      = el.closest( 'label' );
+						var hasAriaLabel     = ( ariaLabel && '' !== ariaLabel ) ? true : false;
+						var hasRealLabel     = ( label && '' !== wpaElementText( label ) ) ? true : false;
+						var hasImplicitLabel = ( parentLabel && '' !== wpaElementText( parentLabel ) ) ? true : false;
+						var hasAriaLabelled  = ( ariaLabeller && '' !== wpaElementText( arialabeller ) ) ? true : false;
+						if ( hasAriaLabel || hasRealLabel || hasImplicitLabel || hasAriaLabelled ) {
+							// This has a label.
+							el.removeAttribute( 'title' );
+						} else {
+							el.setAttribute( 'aria-label', title );
+							el.removeAttribute( 'title' );
+						}
+						break;
+				}
+			}
+		});
+
+	}
+
 	if ( wpa.target ) {
 		$('a:not(.wpa-allow-target)').removeAttr( 'target' );
 	}
@@ -55,3 +113,26 @@
 	}
 
 }(jQuery));
+
+function wpaElementText(el) {
+	var text = '';
+	// Text node (3) or CDATA node (4) - return its text
+	if ( (el.nodeType === 3) || (el.nodeType === 4) ) {
+		text = el.nodeValue;
+	// If node is an element (1) and an img, input[type=image], or area element, return its alt text
+	} else if ( (el.nodeType === 1) && (
+			(el.tagName.toLowerCase() == 'img') ||
+			(el.tagName.toLowerCase() == 'area') ||
+			((el.tagName.toLowerCase() == 'input') && el.getAttribute('type') && (el.getAttribute('type').toLowerCase() == 'image'))
+			) ) {
+		text = el.getAttribute('alt') || '';
+	// Traverse children unless this is a script or style element
+	} else if ( (el.nodeType === 1) && !el.tagName.match(/^(script|style)$/i) ) {
+		var children = el.childNodes;
+		for (var i = 0, l = children.length; i < l; i++) {
+			text += wpaElementText(children[i]);
+		}
+	}
+
+	return text;
+};
