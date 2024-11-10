b8a6a4b7c041645012f7bb2620a22a7e7b199923
Joe Dolson <design@joedolson.com>
2024-11-10 16:01:14 -0600

    Move customization & privacy info to Help page

--- src/wp-accessibility-help.php
+++ src/wp-accessibility-help.php
@@ -40,6 +40,36 @@ function wpa_help_screen() {
 							</div>
 						</div>
 					</div>
+					<div class="postbox">
+						<h2 class='hndle'><?php _e( 'Customization', 'wp-accessibility' ); ?></h2>
+
+						<div class="inside">
+							<p>
+								<?php _e( 'Custom high-contrast styles go in <code>a11y-contrast.css</code> in your Theme\'s stylesheet directory.', 'wp-accessibility' ); ?>
+							</p>
+							<p>
+								<?php _e( 'Set custom styles for large print using the body class <code>.fontsize</code> in your theme styles or the customizer.', 'wp-accessibility' ); ?>
+							</p>
+							<p>
+								<?php _e( 'Set a custom long description template by adding <code>longdesc-template.php</code> to your theme directory.', 'wp-accessibility' ); ?>
+							</p>
+							<p>
+								<?php _e( 'The <a href="#wpa_widget_toolbar">shortcode for the Accessibility toolbar</a> is <code>[wpa_toolbar]</code>', 'wp-accessibility' ); ?>
+							</p>
+						</div>
+					</div>
+					<div class="postbox" id="privacy">
+						<h2 class='hndle'><?php _e( 'Privacy', 'wp-accessibility' ); ?></h2>
+
+						<div class="inside">
+							<h3><?php _e( 'Cookies', 'wp-accessibility' ); ?></h3>
+							<p><?php _e( 'The accessibility toolbar sets cookies to maintain awareness of the user\'s selected accessibility options. If the toolbar is not in use, WP Accessibility does not set any cookies.', 'wp-accessibility' ); ?></p>
+							<h3><?php _e( 'Information collected by WP Accessibility', 'wp-accessibility' ); ?></h3>
+							<p><?php _e( 'WP Accessibility does not collect any personally identifying information about users or visitors.', 'wp-accessibility' ); ?>
+							<?php _e( 'User statistics (toolbar actions) collected by WP Accessibility are tracked using browser fingerprinting, and no identifiable information is stored at any time.', 'wp-accessibility' ); ?>
+							<?php _e( 'Page statistics are collected when the page is initially viewed, then again if the accessibility values have changed since the last check.', 'wp-accessibility' ); ?></p>
+						</div>
+					</div>
 				</div>
 			</div>
 			<?php wpa_admin_sidebar(); ?>
